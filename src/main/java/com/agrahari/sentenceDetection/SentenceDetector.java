1a032d220e70e5f8be3b023e33f67c22198b48a8
Ankit Agrahari <ankit.agrahari@microfocus.com>
2020-07-19 21:02:06 +0530

    Update SentenceDetector.java

--- src/main/java/com/agrahari/sentenceDetection/SentenceDetector.java
+++ src/main/java/com/agrahari/sentenceDetection/SentenceDetector.java
@@ -8,13 +8,13 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class SentenceDetector {
 
     public void detectSentence(String rawText){
         try {
+
             //Load the sentence detection model
             InputStream is = new FileInputStream("src\\main\\resources\\models\\en-sent.bin");
             SentenceModel modelObj = new SentenceModel(is);
@@ -23,9 +23,8 @@ public class SentenceDetector {
             SentenceDetectorME detectorME = new SentenceDetectorME(modelObj);
             String[] detectedSen = detectorME.sentDetect(rawText);
             System.out.println("All sentences detected are:");
-            for(String sen: detectedSen){
-                System.out.println(sen);
-            }
+            System.out.println(Arrays.toString(detectedSen));
+
             //Find position of all sentences
             Span[] spans = detectorME.sentPosDetect(rawText);
             System.out.println("Positions of all detected sentences:");
@@ -35,9 +34,9 @@ public class SentenceDetector {
 
             //Getting the probabilities of the last sendDetect method call for the sentences
             double[] probabilities = detectorME.getSentenceProbabilities();
-            for(double v: probabilities){
-                System.out.println(v);
-            }
+            System.out.println(Arrays.toString(probabilities));
+
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -49,7 +48,7 @@ public class SentenceDetector {
 
         SentenceDetector sd = new SentenceDetector();
         String sentence = "Hi. How are you? Hope everything is going well. " +
-                "Welcome to ChatBot Lesson1. We will try to understand Open Apache NLP for sentence detection";
+                "Welcome to OpenNLP Lesson1. We will try to understand Open Apache NLP for sentence detection";
         sd.detectSentence(sentence);
     }
 }
